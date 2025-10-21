29dcf0ff5ba0eab61b613e1c440b0e1157e85d04
Denis <densaburoff@gmail.com>
2025-10-21 20:58:41 +0300

    FIX: set headless=True for CI/CD

--- conftest.py
+++ conftest.py
@@ -1,14 +1,12 @@
 import pytest
-import os
 from playwright.sync_api import Page, sync_playwright
 
 
 @pytest.fixture(scope="function")
 def page(request):
     with sync_playwright() as p:
-        # В CI/CD запускаем headless, локально - с браузером
-        headless = os.getenv('CI') == 'true'
-        browser = p.chromium.launch(headless=False)
+        # В CI/CD запускаем headless, локально - с брауз
+        browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
         yield page
