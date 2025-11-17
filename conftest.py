5c2815898bf0646584bc569e0228bcbb4ae7893f
Denis <densaburoff@gmail.com>
2025-11-17 22:18:40 +0300

    удален тест с сайта Литрес

--- conftest.py
+++ conftest.py
@@ -5,7 +5,7 @@ from playwright.sync_api import Page, sync_playwright
 @pytest.fixture(scope="function")
 def page(request):
     with sync_playwright() as p:
-        # В CI/CD запускаем headless, локально - с брауз
+        # В CI/CD запускаем headless, локально - с браузером
         browser = p.chromium.launch(headless=True)
         context = browser.new_context()
         page = context.new_page()
--- tests/litres_test.py
+++
@@ -1,9 +0,0 @@
-from playwright.sync_api import Page, expect
-from pages.litres_page import LitresPage
-
-
-def test_visible_lit(page: Page):
-    litres_test = LitresPage(page)
-    litres_test.open()
-    litres_test.click_basket_my()
-    litres_test.visible_words_my_text_is_('Корзина пуста')
